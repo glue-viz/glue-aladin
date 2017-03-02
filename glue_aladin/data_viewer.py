787664042af5b92be12aec003a5e5bef6c6fc6a1
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-03-02 15:07:12 -0500

    Fix color and alpha sync and subscribe to subset messages

--- glue_aladin/data_viewer.py
+++ glue_aladin/data_viewer.py
@@ -1,5 +1,6 @@
 from glue.viewers.common.qt.data_viewer import DataViewer
 from glue.viewers.common.qt.toolbar import BasicToolbar
+from glue.core import message as msg
 
 from glue_aladin.aladin_lite import AladinLiteQtWidget
 from glue_aladin.viewer_state import AladinLiteState
@@ -45,3 +46,38 @@ class AladinLiteViewer(DataViewer):
 
     def options_widget(self):
         return self._options_widget
+
+    def _add_subset(self, message):
+        self.add_subset(message.subset)
+
+    def _update_subset(self, message):
+        if message.subset in self._layer_artist_container:
+            for layer_artist in self._layer_artist_container[message.subset]:
+                layer_artist.update()
+
+    def _remove_subset(self, message):
+        if message.subset in self._layer_artist_container:
+            layer_artist = self._layer_artist_container.pop(message.subset)
+            layer_artist.clear()
+
+    def register_to_hub(self, hub):
+
+        super(AladinLiteViewer, self).register_to_hub(hub)
+
+        def subset_has_data(x):
+            return x.sender.data in self._layer_artist_container.layers
+
+        def has_data(x):
+            return x.sender in self._layer_artist_container.layers
+
+        hub.subscribe(self, msg.SubsetCreateMessage,
+                      handler=self._add_subset,
+                      filter=subset_has_data)
+
+        hub.subscribe(self, msg.SubsetUpdateMessage,
+                      handler=self._update_subset,
+                      filter=subset_has_data)
+
+        hub.subscribe(self, msg.SubsetDeleteMessage,
+                      handler=self._remove_subset,
+                      filter=subset_has_data)
--- glue_aladin/layer_state.py
+++ glue_aladin/layer_state.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
 from glue.core.state_objects import State
-from glue.external.echo import CallbackProperty
+from glue.external.echo import CallbackProperty, keep_in_sync
 
 
 class AladinLiteLayerState(State):
@@ -11,3 +11,29 @@ class AladinLiteLayerState(State):
     zorder = CallbackProperty(0)
     color = CallbackProperty()
     alpha = CallbackProperty()
+
+    def __init__(self, **kwargs):
+
+        super(AladinLiteLayerState, self).__init__(**kwargs)
+
+        self._sync_color = None
+        self._sync_alpha = None
+
+        self.add_callback('layer', self._layer_changed)
+        self._layer_changed()
+
+    def _layer_changed(self):
+
+        if self._sync_color is not None:
+            self._sync_color.stop_syncing()
+
+        if self._sync_alpha is not None:
+            self._sync_alpha.stop_syncing()
+
+        if self.layer is not None:
+
+            self.color = self.layer.style.color
+            self.alpha = self.layer.style.alpha
+
+            self._sync_color = keep_in_sync(self, 'color', self.layer.style, 'color')
+            self._sync_alpha = keep_in_sync(self, 'alpha', self.layer.style, 'alpha')
